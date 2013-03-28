9fd72deab2760979dbca714ec7a2473e795d2529
Tyson Tate <tyson@tysontate.com>
2013-03-27 21:05:33 -0700

    Initial commit.

---
+++ udp.go
@@ -0,0 +1,96 @@
+package udp
+
+import (
+	"net"
+	"strings"
+	"testing"
+)
+
+var (
+	addr     *string
+	listener *net.UDPConn
+)
+
+type fn func()
+
+// SetAddr sets the UDP port that will be listened on.
+func SetAddr(a string) {
+	addr = &a
+}
+
+func start(t *testing.T) {
+	resAddr, err := net.ResolveUDPAddr("udp", *addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+	listener, err = net.ListenUDP("udp", resAddr)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func stop(t *testing.T) {
+	if err := listener.Close(); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func getMessage(t *testing.T, body fn) string {
+	start(t)
+	defer stop(t)
+
+	result := make(chan string)
+
+	go func() {
+		message := make([]byte, 1024)
+		n, _, _ := listener.ReadFrom(message)
+		result <- string(message[0:n])
+	}()
+
+	body()
+
+	return <-result
+}
+
+func get(t *testing.T, match string, body fn) (got string, equals bool, contains bool) {
+	got = getMessage(t, body)
+	equals = got == match
+	contains = strings.Contains(got, match)
+	return got, equals, contains
+}
+
+// ShouldReceive will fire a test error if the given function doesn't send
+// exactly the given string over UDP.
+func ShouldReceive(t *testing.T, expected string, body fn) {
+	got, equals, _ := get(t, expected, body)
+	if !equals {
+		t.Errorf("Expected %#v but got %#v instead", expected, got)
+	}
+}
+
+// ShouldNotReceive will fire a test error if the given function sends exactly
+// the given string over UDP.
+func ShouldNotReceive(t *testing.T, notExpected string, body fn) {
+	_, equals, _ := get(t, notExpected, body)
+	if equals {
+		t.Errorf("Expected not to get %v but did", notExpected)
+	}
+}
+
+// ShouldContain will fire a test error if the given function doesn't send
+// the given string over UDP.
+func ShouldContain(t *testing.T, expected string, body fn) {
+	got, _, contains := get(t, expected, body)
+	if !contains {
+		t.Errorf("Expected to find %#v but got %#v instead", expected, got)
+	}
+}
+
+// ShouldNotContain will fire a test error if the given function sends the
+// given string over UDP.
+func ShouldNotContain(t *testing.T, expected string, body fn) {
+	got, _, contains := get(t, expected, body)
+	if contains {
+		t.Errorf("Expected not to find %#v but got %#v", expected, got)
+	}
+}
---
+++ udp_test.go
@@ -0,0 +1,63 @@
+package udp
+
+import (
+	"net"
+	"testing"
+	"time"
+)
+
+var (
+	testAddr = ":8126"
+)
+
+func TestAll(t *testing.T) {
+	udpClient, err := net.DialTimeout("udp", testAddr, time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	SetAddr(testAddr)
+
+	testValues := [][]interface{}{
+		[]interface{}{"foo", "foo", true, true},
+		[]interface{}{"foo", "bar", false, false},
+		[]interface{}{"foo", "foobar", false, true},
+	}
+
+	for _, values := range testValues {
+		shouldGet := values[0].(string)
+		sendString := values[1].(string)
+		shouldEquals := values[2].(bool)
+		shouldContains := values[3].(bool)
+
+		got, equals, contains := get(t, shouldGet, func() {
+			udpClient.Write([]byte(sendString))
+		})
+
+		if got != sendString {
+			t.Errorf("Should've got %s but got %s", sendString, got)
+		}
+		if equals != shouldEquals {
+			t.Errorf("Equals should've been %s but was %s", shouldEquals, equals)
+		}
+		if contains != shouldContains {
+			t.Errorf("Contains should've been %s but was %s", shouldContains, contains)
+		}
+	}
+
+	ShouldReceive(t, "foo", func() {
+		udpClient.Write([]byte("foo"))
+	})
+
+	ShouldNotReceive(t, "bar", func() {
+		udpClient.Write([]byte("foo"))
+	})
+
+	ShouldContain(t, "foo", func() {
+		udpClient.Write([]byte("barfoo"))
+	})
+
+	ShouldNotContain(t, "bar", func() {
+		udpClient.Write([]byte("fooba"))
+	})
+}
